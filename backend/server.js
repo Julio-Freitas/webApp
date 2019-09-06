6b23681575cafcf670b4a56fd34b40d0377b7a39
Julio Freitas <julio.clf31@gmail.com>
2019-09-06 19:20:22 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 const routes = require('./routes');
 const express = require('express');
+const path = require('path');
+const bodyParse = require('body-parser');
 const cors = require('cors');
 
 const server = express();
 
+const port = process.env.PORT || 3333 ;
 
+server.use(express.static(path.join(__dirname, 'public')));
+server.use(bodyParse.urlencoded({extended: true}));
 
 server.use(express.json());
 server.use(cors()); //permite que a app seja acessada por qualquer endereço
 server.use(routes);
 
-server.listen(3333, () => {
-    console.log('servidor rodando!');
-}); 
 
+server.listen(port, (err) => {
+    if (err) {
+        console.log('Servidor não rodou!');
+    } else {
+        console.log('Servidor rodando');
+    }
+});
